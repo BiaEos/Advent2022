272aa69874402dce4ff2e2bbd2100763bf2bd8ff
Alyson Tremblay <a.tremblay.work@gmail.com>
2022-12-29 11:15:21 -0500

    Add create directory

--- src/Advent2022/DaySeven/SpaceOnDevice.java
+++ src/Advent2022/DaySeven/SpaceOnDevice.java
@@ -10,12 +10,14 @@
 
 package Advent2022.DaySeven;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import static Advent2022.Tools.LaunchProgram.launchProgram;
 import static Advent2022.Tools.LoadFile.inputFromFile;
 
 public class SpaceOnDevice {
+    private static File directory;
     private static ArrayList<String> outputFromTerminal = new ArrayList<>(inputFromFile());
     public static void start() {
         launchProgram("one", "two", SpaceOnDevice.class,
@@ -24,11 +26,27 @@ public class SpaceOnDevice {
 
     public static void startDayOne() {
         System.out.println(outputFromTerminal);
+        createDirectory("TempFolder", "/Users/main/Projects/Advent2022/src/Advent2022/DaySeven/");
     }
 
     public static void startDayTwo() {
         //do day two stuff
     }
 
-
+    private static void createDirectory(String directoryName, String path) {
+        boolean success = false;
+        File directory = new File(path + directoryName);
+        if (directory.exists() && directory.isDirectory()) {
+            System.out.println("Directory already exists");
+        } else {
+            System.out.println("Creating directory");
+            success = directory.mkdir();
+            directory.deleteOnExit();
+            if (success) {
+                System.out.println("Successfully created directory");
+            } else {
+                System.out.println("Failed to create directory");
+            }
+        }
+    }
 }

b613c3aa9cddd714b7c69f9e5eeb3ced2cd4a1df
Alyson Tremblay <a.tremblay.work@gmail.com>
2023-01-04 10:41:17 -0500

    Day 8 Part 1 complete

--- src/Advent2022/DayEight/TreeHouseLocation.java
+++ src/Advent2022/DayEight/TreeHouseLocation.java
@@ -10,13 +10,16 @@
 
 package Advent2022.DayEight;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.*;
 
 import static Advent2022.Tools.LaunchProgram.launchProgram;
 
 public class TreeHouseLocation {
-    private static int[][] heightOfTrees;
+    private static int[][] heightOfTrees = new int[99][99];
+    private static List<String> alreadyMarkedTrees = new ArrayList<String>();
 
     public static void start() {
         launchProgram("one", "two", TreeHouseLocation.class,
@@ -24,21 +27,89 @@ public class TreeHouseLocation {
     }
 
     public static void startDayOne() {
-        System.out.println(Arrays.deepToString(getTreeMap()));
+        getTreeMap();
+        visibleTrees();
     }
 
     public static void startDayTwo() {
         System.out.println("Do day two things");
     }
 
-    private static int[][] getTreeMap() {
-        Scanner scanner = new Scanner(
-                "/Users/main/Projects/Advent2022/src/Advent2022/DayEight/mapHeightOfTrees.txt");
-        for (int row = 0; row < 99; row++) {
-            for (int col = 0; col < 99; col++) {
-                heightOfTrees[row][col] = scanner.nextInt();
+    private static void getTreeMap() {
+        Scanner scanner = null;
+        try {
+            Reader reader = new FileReader("src/Advent2022/DayEight/mapHeightOfTrees.txt");
+            scanner = new Scanner(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        for (int row = 1; row < 100; row++) {
+            String line = Objects.requireNonNull(scanner).nextLine();
+            for (int col = 1; col < 100; col++) {
+                int number = Integer.parseInt(line.substring(col - 1, col));
+                heightOfTrees[row - 1][col - 1] = number;
             }
         }
-        return heightOfTrees;
+    }
+
+    private static void visibleTrees() {
+        // get visible from left side
+        int visible = (99 * 2) + (97 * 2);
+        for (int row = 1; row < 98; row++) {
+            int tallestTreeInRow = heightOfTrees[row][0];
+            for (int col = 1; col < 98; col++) {
+                int currentTree = heightOfTrees[row][col];
+                if (currentTree > tallestTreeInRow) {
+                    tallestTreeInRow = currentTree;
+                    if (!alreadyMarkedTrees.contains(row + "/" + col)) {
+                        alreadyMarkedTrees.add(row + "/" + col);
+                        visible++;
+                    }
+                }
+            }
+        }
+        // get visible from right side
+        for (int row = 97; row > 0; row--) {
+            int tallestTreeInRow = heightOfTrees[row][98];
+            for (int col = 97; col > 0; col--) {
+                int currentTree = heightOfTrees[row][col];
+                if (currentTree > tallestTreeInRow) {
+                    tallestTreeInRow = currentTree;
+                    if (!alreadyMarkedTrees.contains(row + "/" + col)) {
+                        alreadyMarkedTrees.add(row + "/" + col);
+                        visible++;
+                    }
+                }
+            }
+        }
+        // get visible from top side
+        for (int col = 1; col < 98; col++) {
+            int tallestTreeInRow = heightOfTrees[0][col];
+            for (int row = 1; row < 98; row++) {
+                int currentTree = heightOfTrees[row][col];
+                if (currentTree > tallestTreeInRow) {
+                    tallestTreeInRow = currentTree;
+                    if (!alreadyMarkedTrees.contains(row + "/" + col)) {
+                        alreadyMarkedTrees.add(row + "/" + col);
+                        visible++;
+                    }
+                }
+            }
+        }
+        // get visible from bottom side
+        for (int col = 97; col > 0; col--) {
+            int tallestTreeInRow = heightOfTrees[98][col];
+            for (int row = 97; row > 0; row--) {
+                int currentTree = heightOfTrees[row][col];
+                if (currentTree > tallestTreeInRow) {
+                    tallestTreeInRow = currentTree;
+                    if (!alreadyMarkedTrees.contains(row + "/" + col)) {
+                        alreadyMarkedTrees.add(row + "/" + col);
+                        visible++;
+                    }
+                }
+            }
+        }
+        System.out.println("The number of visible trees: " + visible);
     }
 }

22219f19a6cc359ee2d8f351bfe2dba7e165aeb6
Alyson Tremblay <a.tremblay.work@gmail.com>
2023-01-31 12:18:14 -0500

    Day 8 Part 2 complete

--- src/Advent2022/DayEight/TreeHouseLocation.java
+++ src/Advent2022/DayEight/TreeHouseLocation.java
@@ -18,8 +18,10 @@ import java.util.*;
 import static Advent2022.Tools.LaunchProgram.launchProgram;
 
 public class TreeHouseLocation {
-    private static int[][] heightOfTrees = new int[99][99];
-    private static List<String> alreadyMarkedTrees = new ArrayList<String>();
+    private static final int[][] heightOfTrees = new int[99][99];
+    private static final List<Integer> treeScore = new ArrayList<>();
+    private static final List<String> alreadyMarkedTrees = new ArrayList<>();
+    private static int visible = 0;
 
     public static void start() {
         launchProgram("One", "Two", TreeHouseLocation.class,
@@ -29,10 +31,13 @@ public class TreeHouseLocation {
     public static void startDayOne() {
         getTreeMap();
         visibleTrees();
+        printDayOne();
     }
 
     public static void startDayTwo() {
-        System.out.println("Do day two things");
+        getTreeMap();
+        getScenicScore();
+        getHighestScenicScore();
     }
 
     private static void getTreeMap() {
@@ -54,11 +59,13 @@ public class TreeHouseLocation {
 
     private static void visibleTrees() {
         // get visible from left side
-        int visible = (99 * 2) + (97 * 2);
+        visible = (99 * 2) + (97 * 2);
+        int tallestTreeInRow;
+        int currentTree;
         for (int row = 1; row < 98; row++) {
-            int tallestTreeInRow = heightOfTrees[row][0];
+            tallestTreeInRow = heightOfTrees[row][0];
             for (int col = 1; col < 98; col++) {
-                int currentTree = heightOfTrees[row][col];
+                currentTree = heightOfTrees[row][col];
                 if (currentTree > tallestTreeInRow) {
                     tallestTreeInRow = currentTree;
                     if (!alreadyMarkedTrees.contains(row + "/" + col)) {
@@ -70,9 +77,9 @@ public class TreeHouseLocation {
         }
         // get visible from right side
         for (int row = 97; row > 0; row--) {
-            int tallestTreeInRow = heightOfTrees[row][98];
+            tallestTreeInRow = heightOfTrees[row][98];
             for (int col = 97; col > 0; col--) {
-                int currentTree = heightOfTrees[row][col];
+                currentTree = heightOfTrees[row][col];
                 if (currentTree > tallestTreeInRow) {
                     tallestTreeInRow = currentTree;
                     if (!alreadyMarkedTrees.contains(row + "/" + col)) {
@@ -84,9 +91,9 @@ public class TreeHouseLocation {
         }
         // get visible from top side
         for (int col = 1; col < 98; col++) {
-            int tallestTreeInRow = heightOfTrees[0][col];
+            tallestTreeInRow = heightOfTrees[0][col];
             for (int row = 1; row < 98; row++) {
-                int currentTree = heightOfTrees[row][col];
+                currentTree = heightOfTrees[row][col];
                 if (currentTree > tallestTreeInRow) {
                     tallestTreeInRow = currentTree;
                     if (!alreadyMarkedTrees.contains(row + "/" + col)) {
@@ -98,9 +105,9 @@ public class TreeHouseLocation {
         }
         // get visible from bottom side
         for (int col = 97; col > 0; col--) {
-            int tallestTreeInRow = heightOfTrees[98][col];
+            tallestTreeInRow = heightOfTrees[98][col];
             for (int row = 97; row > 0; row--) {
-                int currentTree = heightOfTrees[row][col];
+                currentTree = heightOfTrees[row][col];
                 if (currentTree > tallestTreeInRow) {
                     tallestTreeInRow = currentTree;
                     if (!alreadyMarkedTrees.contains(row + "/" + col)) {
@@ -110,6 +117,99 @@ public class TreeHouseLocation {
                 }
             }
         }
+    }
+
+    private static void printDayOne() {
         System.out.println("The number of visible trees: " + visible);
+    }
+
+    private static void getScenicScore() {
+        for (int row = 0; row < 99; row++) {
+            for (int col = 0; col < 99; col++) {
+                if (!isEndTree(row, col)) {
+                    int left = checkLeft(row, col);
+                    int right = checkRight(row, col);
+                    int up = checkUp(row, col);
+                    int down = checkDown(row, col);
+                    treeScore.add(left * right * down * up);
+                }
+            }
+        }
+    }
+
+    private static boolean isEndTree(int row, int col) {
+        return row == 0 || row == 98 || col == 0 || col == 98;
+    }
+
+    private static int checkLeft(int row, int col) {
+        int leftCount = 1;
+        int treeHeight = heightOfTrees[row][col];
+        int nextTreeHeight = 0;
+        while (nextTreeHeight < treeHeight) {
+            if ((col - 1) > 0) {
+                col -= 1;
+                nextTreeHeight = heightOfTrees[row][col];
+                if (nextTreeHeight < treeHeight) {
+                    leftCount++;
+                }
+            }
+            else { break; }
+        }
+        return leftCount;
+    }
+
+    private static int checkRight(int row, int col) {
+        int rightCount = 1;
+        int treeHeight = heightOfTrees[row][col];
+        int nextTreeHeight = 0;
+        while (nextTreeHeight < treeHeight) {
+            if ((col + 1) < 98) {
+                col += 1;
+                nextTreeHeight = heightOfTrees[row][col];
+                if (nextTreeHeight < treeHeight) {
+                    rightCount++;
+                }
+            }
+            else { break; }
+        }
+        return rightCount;
+    }
+
+    private static int checkUp(int row, int col) {
+        int upCount = 1;
+        int treeHeight = heightOfTrees[row][col];
+        int nextTreeHeight = 0;
+        while (nextTreeHeight < treeHeight) {
+            if ((row - 1) > 0) {
+                row -= 1;
+                nextTreeHeight = heightOfTrees[row][col];
+                if (nextTreeHeight < treeHeight) {
+                    upCount++;
+                }
+            }
+            else { break; }
+        }
+        return upCount;
+    }
+
+    private static int checkDown(int row, int col) {
+        int downCount = 1;
+        int treeHeight = heightOfTrees[row][col];
+        int nextTreeHeight = 0;
+        while (nextTreeHeight < treeHeight) {
+            if ((row + 1) < 98) {
+                row += 1;
+                nextTreeHeight = heightOfTrees[row][col];
+                if (nextTreeHeight < treeHeight) {
+                    downCount++;
+                }
+            }
+            else { break; }
+        }
+        return downCount;
+    }
+
+    private static void getHighestScenicScore() {
+        System.out.println("Max scenic score is: " + Collections.max(treeScore));
     }
 }
